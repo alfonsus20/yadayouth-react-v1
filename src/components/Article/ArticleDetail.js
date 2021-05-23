75de8ce02a5901c95daeaa62614146615cc54678
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-05-24 01:47:05 +0800

    Add min h screen to article page

--- src/components/Article/ArticleDetail.js
+++ src/components/Article/ArticleDetail.js
@@ -14,7 +14,8 @@ import { countWords } from "../../utils/functions";
 const ArticleDetail = () => {
   const { id } = useParams();
   const [article, setArticle] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [articleLoading, setArticleLoading] = useState(false);
+  const [otherArticlesloading, setOtherArticlesLoading] = useState(false);
   const [articles, setArticles] = useState([]);
 
   const override = css`
@@ -28,14 +29,14 @@ const ArticleDetail = () => {
       try {
         let response = await fetch(article.articleImage);
         let blob = await response.blob();
-        let file = new File([blob], 'image.jpg', {type : blob.type});
+        let file = new File([blob], "image.jpg", { type: blob.type });
         await navigator.share({
-          title: 'Yada Youth Article',
+          title: "Yada Youth Article",
           text: window.document.title,
           url: window.document.location,
-          files : [file]
+          files: [file],
         });
-        console.log("Thank you for sharing !")
+        console.log("Thank you for sharing !");
       } catch (e) {
         console.log(e);
       }
@@ -47,23 +48,25 @@ const ArticleDetail = () => {
 
     const fetchArticle = async () => {
       try {
-        setLoading(true);
+        setArticleLoading(true);
         const { data } = await yadayouth.get(`/api/article/${id}`);
         setArticle(data);
+        setArticleLoading(false);
         document.title = data.title;
         console.log(data);
       } catch (e) {
-        setLoading(false);
+        setArticleLoading(false);
       }
     };
 
     const fetchArticles = async () => {
       try {
+        setOtherArticlesLoading(true);
         const { data } = await yadayouth.get("/api/article/");
         setArticles(data.results);
-        setLoading(false);
+        setOtherArticlesLoading(false);
       } catch (e) {
-        setLoading(false);
+        setOtherArticlesLoading(false);
       }
     };
 
@@ -73,95 +76,115 @@ const ArticleDetail = () => {
   }, [id]);
 
   const readingTime = Math.round(countWords(article.content || "") / 275);
-  // Object.keys(article).length !== 0 ?
   return (
-    <Container maxWidth={1800} additional="mx-auto">
-      <Button scrollUp />
-      {!loading ? (
+    <Container maxWidth={1800} additional="mx-auto" >
+      {!articleLoading ? (
         <>
+          <Button scrollUp />
           <>
-            <h1 className="text-5xl text-blue font-book text-center">
-              {article.title}
-            </h1>
-            <p className="text-center my-4 font-light">
-              {moment(article.timePublised).format("MMMM Do")}, {readingTime}{" "}
-              min read
-            </p>
-            <img
-              src={article.articleImage}
-              className="w-full my-4"
-              alt="article"
-            />
-            <div className="font-book text-lg my-8">
-              <div dangerouslySetInnerHTML={{ __html: article.content }} />
-            </div>
-            <div className="flex flex-col md:flex-row">
-              <div className="flex flex-row flex-wrap w-full sm:w-8/12 md:w-10/12">
-                <span className="font-book p-2 bg-gray rounded-md m-2">
-                  {article.topic}
-                </span>
+            <>
+              <h1 className="text-5xl text-blue font-book text-center">
+                {article.title}
+              </h1>
+              <p className="text-center my-4 font-light">
+                {moment(article.timePublised).format("MMMM Do")}, {readingTime}{" "}
+                min read
+              </p>
+              <img
+                src={article.articleImage}
+                className="w-full my-4"
+                alt="article"
+              />
+              <div className="font-book text-lg my-8">
+                <div dangerouslySetInnerHTML={{ __html: article.content }} />
               </div>
-              <div className="text-white ml-auto mt-4 sm:mt-0 w-full sm:w-4/12 md:w-2/12 grid place-items-center">
-                <div
-                  className="rounded-full py-2 px-4 mb-auto mt-2 icon cursor-pointer"
-                  style={{ backgroundColor: "#FDAE5A" }}
-                  onClick={shareArticle}
-                >
-                  <i className="fas fa-link"></i>&nbsp; Share This Article
+              <div className="flex flex-col md:flex-row justify-between">
+                <div className="flex flex-row flex-wrap">
+                  <span className="font-book p-2 bg-gray rounded-md m-2">
+                    {article.topic}
+                  </span>
+                </div>
+                <div className="text-white mt-4 sm:mt-0 w-full sm:w-4/12 md:w-2/12 grid place-items-center">
+                  <div
+                    className="rounded-full py-2 px-4 mb-auto mt-2 icon cursor-pointer"
+                    style={{ backgroundColor: "#FDAE5A" }}
+                    onClick={shareArticle}
+                  >
+                    <i className="fas fa-link"></i>&nbsp; Share This Article
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <h3 className="text-blue text-center text-xl my-8">
-                More From YadaYouth Article
-              </h3>
-              <div className="flex flex-row flex-wrap ">
-                {articles.slice(0, 3).map((recentArticle) => (
-                  <ArticleMiniCard
-                    key={recentArticle.id}
-                    title={recentArticle.title}
-                    time={moment(recentArticle.timePublised).format(
-                      "dddd, MMMM Do YYYY"
-                    )}
-                    id={recentArticle.id}
-                    image={recentArticle.articleImage}
-                  />
-                ))}
-              </div>
-            </div>
-            <div
-              className="flex justify-between sm:px-12 my-4"
-              style={{ fontSize: "clamp(12px,2vw,1rem)" }}
-            >
-              <Link to="/articles">
-                <div className="bg-orange text-white text-md px-4 py-2 rounded-lg shadow-lg">
-                  <i className="fas fa-chevron-left"></i>
-                  <i className="fas fa-chevron-left"></i>
-                  &nbsp; Previous Article
-                </div>
-              </Link>
-              <Link to="/articles">
-                <div className="bg-orange text-white text-md px-4 py-2 rounded-lg shadow-lg">
-                  Next Article &nbsp;
-                  <i className="fas fa-chevron-right"></i>
-                  <i className="fas fa-chevron-right"></i>
-                </div>
-              </Link>
-            </div>
+            </>
           </>
         </>
       ) : (
-        <div className="grid place-items-center h-60">
+        <div className="grid place-items-center min-h-screen">
           <div className="w-full">
             <div className="text-2xl text-orange text-center mb-2">
               Loading...
             </div>
             <div className="w-full h-24 relative">
-              <PuffLoader loading={loading} color="#FF4C2E" css={override} />
+              <PuffLoader
+                loading={articleLoading}
+                color="#FF4C2E"
+                css={override}
+              />
             </div>
           </div>
         </div>
       )}
+      <div>
+        <h3 className="text-blue text-center text-xl my-4">
+          More From YadaYouth Article
+        </h3>
+        {!otherArticlesloading ? (
+          <div className="flex flex-row flex-wrap ">
+            {articles.slice(0, 3).map((recentArticle) => (
+              <ArticleMiniCard
+                key={recentArticle.id}
+                title={recentArticle.title}
+                time={moment(recentArticle.timePublised).format(
+                  "dddd, MMMM Do YYYY"
+                )}
+                id={recentArticle.id}
+                image={recentArticle.articleImage}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="w-full">
+            <div className="text-2xl text-orange text-center mb-2">
+              Loading...
+            </div>
+            <div className="w-full h-24 relative">
+              <PuffLoader
+                loading={articleLoading}
+                color="#FF4C2E"
+                css={override}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      <div
+        className="flex justify-between sm:px-12 my-4"
+        style={{ fontSize: "clamp(12px,2vw,1rem)" }}
+      >
+        <Link to="/articles">
+          <div className="bg-orange text-white text-md px-4 py-2 rounded-lg shadow-lg">
+            <i className="fas fa-chevron-left"></i>
+            <i className="fas fa-chevron-left"></i>
+            &nbsp; Previous Article
+          </div>
+        </Link>
+        <Link to="/articles">
+          <div className="bg-orange text-white text-md px-4 py-2 rounded-lg shadow-lg">
+            Next Article &nbsp;
+            <i className="fas fa-chevron-right"></i>
+            <i className="fas fa-chevron-right"></i>
+          </div>
+        </Link>
+      </div>
     </Container>
   );
 };
