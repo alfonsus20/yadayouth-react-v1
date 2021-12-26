91ee8e9a7188a7fe3953bb5b8676108ab0e9a3c2
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-12-26 22:46:58 +0800

    redesign homepage and footer

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ image, title, body, link }) => {
   return (
     <Link
       to={link}
-      className="flex flex-col px-4 py-8 bg-yellow w-72 text-xl mx-auto mt-8 rounded-lg border-4 border-orange h-auto"
+      className="flex flex-col px-4 py-8 bg-orange-light w-72 text-xl mx-auto mt-8 rounded-lg border-4 border-orange h-auto"
     >
       <img src={image} alt={title} className="mx-auto h-3/5" />
       <h4 className="font-medium my-1">{title}</h4>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Container from "../Container";
-import { MENUS, SOCMED } from "../../utils/constants";
+import { SOCMED } from "../../utils/constants";
 import FooterLink from "./FooterLink";
 
 const Footer = () => {
   return (
-    <Container
-      bgColor="blue"
-      additional="flex flex-row flex-wrap justify-around text-white text-lg "
-      padding="3% 7%"
+    <div
+      className="bg-blue flex flex-row flex-wrap justify-between text-white text-lg p-12"
       id="contacts"
     >
-      <div className="flex flex-col w-full sm:w-6/12 my-8">
+      <div className="flex flex-col my-8">
         <img
           src="/pictures/yadayouthmainlogo.svg"
           alt="Yada Youth Main Logo"
@@ -20,22 +18,9 @@ const Footer = () => {
         <p className="font-book text-lg">yadayouth.id</p>
         <p className="font-bold text-3xl">Yada Youth Indonesia</p>
         <p className="font-medium text-base">Empowering Children of Tomorrow</p>
-        <p className="font-light text-xs">Copyright &copy; 2021</p>
+        <p className="font-light text-xs">Copyright &copy; 2022</p>
       </div>
-      <div className="flex flex-col w-6/12 sm:w-3/12 justify-between my-8">
-        <h3 className="font-medium">Navigation</h3>
-        <dl>
-          {MENUS.map((menu, id) => (
-            <FooterLink
-              key={id}
-              text={menu.name}
-              pathname={menu.pathname}
-              hash={menu.hash}
-            />
-          ))}
-        </dl>
-      </div>
-      <div className="flex flex-col w-6/12 sm:w-3/12 justify-between h-auto  my-8">
+      <div className="flex flex-col justify-between my-8">
         <h3 className="font-medium">Contact Us</h3>
         <dl className="flex flex-col justify-between  space-y-6">
           {SOCMED.map((socmed, id) => (
@@ -49,7 +34,7 @@ const Footer = () => {
           ))}
         </dl>
       </div>
-    </Container>
+    </div>
   );
 };
 
